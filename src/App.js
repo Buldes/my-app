73a22baf549b8fa9cc056a8fb14b6bdd7aced46e
Buldes <kev.moenig@gmail.com>
2023-02-16 11:59:51 +0100

    Add Pi chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState , useEffect} from 'react';
 import './App.css';
-import { PieChart, Pie, Sector, Cell} from 'recharts';
+import { VictoryPie } from 'victory'
 
 
 function App() {
@@ -14,28 +14,36 @@ function App() {
   const [monthAvarge, setMonthAvarge] = useState(15.33)
   const [monthCost, setMonthCost] = useState(10.67)
   const [monthIncomm, setMonthIncomm] = useState(20)
-  const data01 = [
-    { name: 'Audgaben', value: 400, color:"red"},
-    { name: 'Einkommen', value: 300 , color:"red"},
-  ]
+  const data = [
+    { name: 'Ausgaben', value: 400},
+    { name: 'Einkommen', value: 300 },]
+  const COLORS = ["#ff0000", "#00ff00"]
 
   return (
     <div className='App'>
       <div className='up-area' style={{backgroundColor:"#1f1f1f", width:"100%", height:500}}>
         <div style={{ position:"relative", top:100, ...zentrierungX}}>
-          <label style={{ background: "#3e3e3e", fontSize: "50px", color: "white", borderRadius: "50%", height:250, width: 250, ...defaultLook1 }}>{dailyMoney}€</label>
+          <label style={{ background: "#3e3e3e", fontSize: "60px", color: "white", borderRadius: "50%", height:300, width: 300, ...defaultLook1 }}>{dailyMoney}€</label>
           </div>
-          <label style={{fontSize: "25px", position: "relative", top:-80, left:"65%", backgroundColor:"#5e5e5e", width:300, borderRadius:"20px", ...defaultLook1}}>Ausgaben</label>
-        <div className="Small-Info" style={{ position: "relative", height: 100, width: 300, left: "65%", top: -70, backgroundColor: "#5e5e5e", borderRadius: "10px"}}>
+          <label style={{fontSize: "25px", position: "relative", top:-110, left:"65%", backgroundColor:"#5e5e5e", width:300, borderRadius:"20px", ...defaultLook1}}>Ausgaben</label>
+        <div className="Small-Info" style={{ position: "relative", height: 100, width: 300, left: "65%", top: -100, backgroundColor: "#5e5e5e", borderRadius: "10px"}}>
           <label style={{ fontSize: "25px", position: "relative", top: 10, left: 20, width: 100, height: 30, backgroundColor: "transperent", textAlign: "left", whiteSpace: "nowrap", ...defaultLook2 }}>Durchschnitt:  {monthAvarge}€</label>
           <label style={{ fontSize: "25px", position: "relative", top: 60, left: -210, width: 100, height: 30, backgroundColor: "transperent", textAlign: "left", whiteSpace: "nowrap", ...defaultLook2 }}>Diesen Monat:  {monthCost}€</label>
         </div>
-      <div>
-      <PieChart width={730} height={250}>
-        <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} innerRadius={80} fill="#8884d8" label/>
-      </PieChart>
-      </div>
-    </div>
+      <div className='Pi Chart' style={{backgroundColor:"#1f1f1f", position: "relative", top:-340, left:"10%", transform: "flex", width:500, height: 350, borderRadius: "25px"}}>
+        <VictoryPie
+          data={[
+          { x: "Ausgaben", y: monthCost },
+          { x: "Einkommen", y: monthIncomm}]}
+          animate={{duration:2000}}
+          innerRadius = {80}
+          cornerRadius = {"10"}
+          colorScale={["#9f2f1f", "#2f9f1f"]}
+          style={{
+            data: {fillOpacity: 1, stroke: "#1f1f1f", strokeWidth: 2 },
+            labels: {fontSize: 20, fill: "#ffffff"} ,transform: "flex"}}/>
+        </div>
+     </div>
     </div>
   );
 }
