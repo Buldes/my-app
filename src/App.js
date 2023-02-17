c3eb16430f1c92f3fb22c71419e0eb5b294320c7
Buldes <kev.moenig@gmail.com>
2023-02-17 10:08:17 +0100

    add pichart lable, dailylistitem, down-arrear, bg

--- src/App.js
+++ src/App.js
@@ -14,36 +14,86 @@ function App() {
   const [monthAvarge, setMonthAvarge] = useState(15.33)
   const [monthCost, setMonthCost] = useState(10.67)
   const [monthIncomm, setMonthIncomm] = useState(20)
-  const data = [
-    { name: 'Ausgaben', value: 400},
-    { name: 'Einkommen', value: 300 },]
   const COLORS = ["#ff0000", "#00ff00"]
+
+  function monthCostSet(event){
+    setMonthCost(event.target.value)}
+  function monthIncommSet(event){
+    setMonthIncomm(event.target.value)}
+
+
+  function dailyListItem(props){
+      return (
+      <div style={{height: 35, width: 600, borderRadius: "5px", backgroundColor: "#2e2e2e", display: 'flex', justifyContent: 'flex-start', ...defaultLook1}} >
+        <label style={{backgroundColor: props.state === 'Einkommen' ? '#2f9f1f' : '#ef2f2f', width:150, borderRadius: "3px", fontSize:"22px", textAlign: "middle", ...defaultLook2}}>{props.state}</label>
+        <label style={{backgroundColor: "#5e5e5e", width:180, borderRadius: "3px", fontSize:"22px", textAlign: "middle", position: "relative", left: 5, ...defaultLook2}}>{props.date}</label>
+        <label style={{backgroundColor: "#5e5e5e", width:200, borderRadius: "3px", fontSize:"22px", textAlign: "middle", position: "relative", left:10, ...defaultLook2}}>{props.amount} €  </label>
+      </div>
+  )}
+  
+
+
+class Counter extends React.Component { render() { 
+  return (
+    <div>
+      <button onClick={() => this.props.minus} className="Counter">+</button>
+      <button onClick={() => this.props.minus} className="Counter">-</button>
+    </div>
+    );
+  }
+}
+  
+class Count extends React.Component {constructor(props) { super(); this.state = {numb: Array(2).fill(0),};}
+}
+
+
+
+
 
   return (
     <div className='App'>
-      <div className='up-area' style={{backgroundColor:"#1f1f1f", width:"100%", height:500}}>
+      <div className='up-area' style={{backgroundColor:"#1f1f1f", width:"calc(100% - 10px)", height:500, borderRadius: "25px", position: "relative", top: 5, margin: "0 5px"}}>
+
         <div style={{ position:"relative", top:100, ...zentrierungX}}>
           <label style={{ background: "#3e3e3e", fontSize: "60px", color: "white", borderRadius: "50%", height:300, width: 300, ...defaultLook1 }}>{dailyMoney}€</label>
           </div>
           <label style={{fontSize: "25px", position: "relative", top:-110, left:"65%", backgroundColor:"#5e5e5e", width:300, borderRadius:"20px", ...defaultLook1}}>Ausgaben</label>
+        
         <div className="Small-Info" style={{ position: "relative", height: 100, width: 300, left: "65%", top: -100, backgroundColor: "#5e5e5e", borderRadius: "10px"}}>
           <label style={{ fontSize: "25px", position: "relative", top: 10, left: 20, width: 100, height: 30, backgroundColor: "transperent", textAlign: "left", whiteSpace: "nowrap", ...defaultLook2 }}>Durchschnitt:  {monthAvarge}€</label>
           <label style={{ fontSize: "25px", position: "relative", top: 60, left: -210, width: 100, height: 30, backgroundColor: "transperent", textAlign: "left", whiteSpace: "nowrap", ...defaultLook2 }}>Diesen Monat:  {monthCost}€</label>
         </div>
-      <div className='Pi Chart' style={{backgroundColor:"#1f1f1f", position: "relative", top:-340, left:"10%", transform: "flex", width:500, height: 350, borderRadius: "25px"}}>
-        <VictoryPie
-          data={[
-          { x: "Ausgaben", y: monthCost },
-          { x: "Einkommen", y: monthIncomm}]}
-          animate={{duration:2000}}
-          innerRadius = {80}
-          cornerRadius = {"10"}
-          colorScale={["#9f2f1f", "#2f9f1f"]}
-          style={{
-            data: {fillOpacity: 1, stroke: "#1f1f1f", strokeWidth: 2 },
-            labels: {fontSize: 20, fill: "#ffffff"} ,transform: "flex"}}/>
+        <div className='Pi Chart' style={{backgroundColor:"#2e2e2e", position: "relative", top:-340, left:"10%", transform: "flex", width:500, height: 350, borderRadius: "25px"}}>
+          <div>
+            <label style={{fontSize: "22px", width: 300, height: 32, backgroundColor: "#5e5e5e", position: "relative", left:"20%", top: 10, borderRadius: "22px", ...defaultLook1}}>Diesen Monat</label>
+          </div>
+          <VictoryPie
+            data={[
+            { x: "Ausgaben", y: parseFloat(monthCost)},
+            { x: "Einkommen", y: parseFloat(monthIncomm)}]}
+            animate={{duration:2000}}
+            innerRadius={80}
+            cornerRadius={10}
+            colorScale={["#9f2f1f", "#2f9f1f"]}
+            style={{
+              data: {fillOpacity: 1, stroke: "#2e2e2e", strokeWidth: 3 },
+              labels: {fontSize: 20, fill: "#ffffff"} ,transform: "flex"}}/>
         </div>
+      <div className='down-arear' style={{backgroundColor: "#2f2f2f", width: "100%", height: 10000, top: -270, position: "relative", borderRadius: "25px"}}>
+            <div className='List' style={{display: "flex", backgroundColor: "red", position: "relative", width:500, height: 500, left:"12%", top:60, borderRadius: "10px"}}>
+              <label style={{backgroundColor: "#5e5e5e", width:400, height:35, borderRadius: "25px", fontSize:"22px", textAlign: "middle", position: "relative", top: 10, ...defaultLook1}}>Ausgaben und Einkommen</label>
+              </div>  
+      </div>
      </div>
+
+
+
+
+     
+     <div>
+      
+     </div>
+
     </div>
   );
 }
